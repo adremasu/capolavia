29423469071963a204ab6c6ea316d05957bb3848
Andrea Marchetto <adremasu@gmail.com>
2017-12-20 17:05:41 +0100

    mailchimp integration

--- wp-content/themes/beyond-magazine/header.php
+++ wp-content/themes/beyond-magazine/header.php
@@ -38,6 +38,7 @@
     <meta name="twitter:image" content="<?php echo $url; ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head(); ?>
+    <script id="mcjs">!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/36c7b604eabfd0f01c09956bc/d345499b43b5ac1af79d4d689.js");</script>
 </head>
 <?php $products_archive=get_post_type_archive_link( 'products' );?>
 <?php
