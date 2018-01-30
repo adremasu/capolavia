441e9a82e2ff38c5a4fbcb3ed076c4ce9e505f2c
Andrea Marchetto <adremasu@gmail.com>
2018-01-30 14:19:48 +0100

    un altro file

--- wp-content/themes/beyond-magazine/libs/pdf-bookings.php
+++ wp-content/themes/beyond-magazine/libs/pdf-bookings.php
@@ -208,8 +208,27 @@ if ($date) {
         echo $CSVproductsQts;
     } elseif ($format == 'ODS'){
       if (class_exists('GoogleSheets')){
-        $file = new GoogleSheets();
-      }
+        // Get the API client and construct the service object.
+        $
+        $client = getClient();
+        $service = new Google_Service_Sheets($client);
+
+        // Prints the names and majors of students in a sample spreadsheet:
+        // https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+        $spreadsheetId = '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms';
+        $range = 'Class Data!A2:E';
+        $response = $service->spreadsheets_values->get($spreadsheetId, $range);
+        $values = $response->getValues();
+
+        if (count($values) == 0) {
+          print "No data found.\n";
+        } else {
+          print "Name, Major:\n";
+          foreach ($values as $row) {
+            // Print columns A and E, which correspond to indices 0 and 4.
+            printf("%s, %s\n", $row[0], $row[4]);
+          }
+        }      }
 
     }
 
